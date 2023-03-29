9913f1eb03e740c0783d21f9eada84800bc2e2ab
bhawna chauhan <bhawnabc2002@gmail.com>
2023-03-30 00:32:00 +0530

    Updated the Get yourself Link

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,13 +3,16 @@ import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
   const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate('/jobs', { replace: true });
+  };
 
   return (
     <div className="flex h-full flex-col items-center justify-center">
-      <Title order={3}>404</Title>
-      <Text>Page not found!</Text>
-      <Button className="my-4" onClick={() => navigate(',', { replace: true })}>
-        Back Home
+      <Title order={3}>What are you doing here !</Title>
+      <Text>Well this is sad the page that you were looking for is not here</Text>
+      <Button className="my-4" onClick={handleButtonClick}>
+        Get Yourself Home
       </Button>
     </div>
   );
