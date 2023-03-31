d63a0e2f0690b13fad684728fde59933d2c3bd9c
kunal00000 <kunalverma2468@gmail.com>
2023-04-01 01:34:12 +0530

    fix: linting error

--- src/components/LoadingFallback.tsx
+++ src/components/LoadingFallback.tsx
@@ -4,7 +4,7 @@ import mantisImage from '@/assets/images/mantis-logo-full-transparent.png';
 
 function LoadingFallback() {
   return (
-    <div className="flex h-full animate-fade-in flex-col items-center justify-center gap-4">
+    <div className="animate-fade-in flex h-full flex-col items-center justify-center gap-4">
       <img src={mantisImage} className="h-74 w-80" alt="Mantis Logo" />
       <Loader variant="bars" />
     </div>
